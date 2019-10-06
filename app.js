b103ce448a815378b03a0aa055520176f1cd4ed9
Maciej Kuśnierz <>
2019-10-06 16:36:49 +0200

    Added GCloud confirguration file. Added CORS

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var customersRouter = require('./routes/customers');
 
 var app = express();
-
+var corsOptions = {
+    origin: function(origin, callback) {
+        callback(null, true);
+    },
+    optionsSuccessStatus: 200
+  }
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
